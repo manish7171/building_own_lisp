9f5e007d1ba2e2538a33457fdaf562e85ed4abda
manish <manish7171@gmail.com>
2026-02-21 17:40:30 +0100

    eval the ast

--- main.c
+++ main.c
@@ -28,6 +28,41 @@ void add_history(char *unused) {}
 #include <readline/readline.h>
 #endif
 
+long eval_op(long x, char *op, long y) {
+  if (strcmp(op, "+") == 0) {
+    return x + y;
+  }
+  if (strcmp(op, "-") == 0) {
+    return x - y;
+  }
+  if (strcmp(op, "*") == 0) {
+    return x * y;
+  }
+  if (strcmp(op, "y") == 0) {
+    return x / y;
+  }
+
+  return 0;
+}
+
+long eval(mpc_ast_t *t) {
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  char *op = t->children[1]->contents;
+
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
 int main(int argc, char **argv) {
   mpc_parser_t *Number = mpc_new("number");
   mpc_parser_t *Operator = mpc_new("operator");
@@ -52,13 +87,15 @@ int main(int argc, char **argv) {
     add_history(input);
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-        mpc_ast_print(r.output);
-        mpc_ast_delete(r.output);
+       mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
+      mpc_ast_delete(r.output);
     } else {
-        mpc_err_print(r.error);
-        mpc_err_delete(r.error);
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
     }
-    //printf("No you're a %s", input);
+    // printf("No you're a %s", input);
     free(input);
   }
 
